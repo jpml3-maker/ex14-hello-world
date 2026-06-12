a66e2a0f91698921cdab1ba9b27d1e25ea712e8e
jpml3-maker <jpml3@aluno.ifal.edu.br>
2026-06-12 15:23:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
 
-// TODO: implemente a rota GET /
-// A resposta deve conter a palavra "Hello"
+const express = require('express');
+const app = express();
+
+// TODO: Implemente as rotas:
+// GET /       → 200, corpo contém "Home"
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.status(200).send('Home');
+});
+// GET /sobre  → 200, corpo contém "Sobre"
+app.get('/sobre', (req, res) => {
+    res.status(200).send('Sobre');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+// GET /contato→ 200, corpo contém "Contato"
+app.get('/contato', (req, res) => {
+    res.status(200).send('Contato');
 });
+// Rotas não mapeadas → 404 automático do Express
+
+app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
